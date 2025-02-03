e00569c82ab7a10ab1ff78ad93408d540592e627
SaranRaj <saranraj.1803164@srec.ac.in>
2025-02-03 12:03:41 +0530

    backend Url added to vite.config.js file as proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000/',
+      '/api': 'https://bus-booking-backend-5nwu.onrender.com',
     },
   },
 })
